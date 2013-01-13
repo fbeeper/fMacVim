10ae0a219c95e46ee4b3fd7a2913e8710609b40d
fbeeper <fbeeper@gmail.com>
2013-01-13 20:28:39 +0100

    ups, solving another error from my clumpsy fingers

--- fvim.sh
+++ fvim.sh
@@ -68,7 +68,7 @@ then
 			echo -ne "\x1B[00m"
 			if [[ -f $clng_cmd ]]
 			then
-				echo -ne "Compiling MacVim (by Björn Winckler)\n"
+				echo -ne "Compiling MacVim (by Björn Winckler)"
 				git clone git://github.com/b4winckler/macvim.git /tmp/macvim
 				cd /tmp/macvim/src
 				CC=clang ./configure --with-features=huge \
@@ -147,103 +147,103 @@ ln -s $mvim_internals/vimrc ~/.vimrc
 echo -ne "   \x1B[00;32mDONE\x1B[00m\n"
 
 
-#echo -e "\n2) \x1B[00;34mInstall plugins:\x1B[00m\n"
-#mkdir -p ~/.vim/autoload ~/.vim/bundle
-#echo -e "   * \x1B[00;36mpathogen.vim\x1B[00m (super easy plugin management)"
-#echo "     by Tim Pope"
-#curl -Sso ~/.vim/autoload/pathogen.vim \
-#    https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
-###cd /tmp/
-###git clone git://github.com/tpope/vim-pathogen.git
-###cp /tmp/autoload/pathogen.vim ~/.vim/autoload/
-###rm -rf /tmp/auto
-#echo -ne "     \x1B[00;32mDONE\x1B[00m\n"
+echo -e "\n2) \x1B[00;34mInstall plugins:\x1B[00m\n"
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+echo -e "   * \x1B[00;36mpathogen.vim\x1B[00m (super easy plugin management)"
+echo "     by Tim Pope"
+curl -Sso ~/.vim/autoload/pathogen.vim \
+    https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+##cd /tmp/
+##git clone git://github.com/tpope/vim-pathogen.git
+##cp /tmp/autoload/pathogen.vim ~/.vim/autoload/
+##rm -rf /tmp/auto
+echo -ne "     \x1B[00;32mDONE\x1B[00m\n"
 
-#cd ~/.vim/bundle
-#echo -e "\n   * \x1B[00;36mfugitive.vim\x1B[00m (nice git integration)"
-#echo "     by Tim Pope"
-#git clone git://github.com/tpope/vim-fugitive.git
-#echo -ne "     \x1B[00;32mDONE\x1B[00m\n"
+cd ~/.vim/bundle
+echo -e "\n   * \x1B[00;36mfugitive.vim\x1B[00m (nice git integration)"
+echo "     by Tim Pope"
+git clone git://github.com/tpope/vim-fugitive.git
+echo -ne "     \x1B[00;32mDONE\x1B[00m\n"
 
-#echo -e "\n   * \x1B[00;36mGitv\x1B[00m ('gitk clone' plugin for fugitive)"
-#echo "     by Greg Sexton"
-#git clone git://github.com/gregsexton/gitv.git
-#echo -ne "     \x1B[00;32mDONE\x1B[00m\n"
+echo -e "\n   * \x1B[00;36mGitv\x1B[00m ('gitk clone' plugin for fugitive)"
+echo "     by Greg Sexton"
+git clone git://github.com/gregsexton/gitv.git
+echo -ne "     \x1B[00;32mDONE\x1B[00m\n"
 
-#echo -e "\n   * \x1B[00;36mSolarized\x1B[00m (pleasant colorscheme)"
-#echo "     by Ethan Schoonover"
-#git clone git://github.com/altercation/vim-colors-solarized.git
-#echo -ne "     \x1B[00;32mDONE\x1B[00m\n"
+echo -e "\n   * \x1B[00;36mSolarized\x1B[00m (pleasant colorscheme)"
+echo "     by Ethan Schoonover"
+git clone git://github.com/altercation/vim-colors-solarized.git
+echo -ne "     \x1B[00;32mDONE\x1B[00m\n"
 
-#echo -e "\n   * \x1B[00;36mNERDTree\x1B[00m (useful file system explorer)" 
-#echo "     by Martin Grenfell" 
-#git clone git://github.com/scrooloose/nerdtree.git 
-#echo -ne " \x1B[00;32mDONE\x1B[00m\n"
+echo -e "\n   * \x1B[00;36mNERDTree\x1B[00m (useful file system explorer)" 
+echo "     by Martin Grenfell" 
+git clone git://github.com/scrooloose/nerdtree.git 
+echo -ne " \x1B[00;32mDONE\x1B[00m\n"
 
-#echo -e "\n   * \x1B[00;36mNERDCommenter\x1B[00m (comment your code easily ,cl ,cu)"
-#echo "     by Martin Grenfell (again :P)"
-#git clone git://github.com/scrooloose/nerdcommenter.git
-#echo -ne "     \x1B[00;32mDONE\x1B[00m\n"
+echo -e "\n   * \x1B[00;36mNERDCommenter\x1B[00m (comment your code easily ,cl ,cu)"
+echo "     by Martin Grenfell (again :P)"
+git clone git://github.com/scrooloose/nerdcommenter.git
+echo -ne "     \x1B[00;32mDONE\x1B[00m\n"
 
-#echo -e "\n   * \x1B[00;36mPowerline\x1B[00m (cool statusline replacement)"
-#echo "     by Kim Silkebækken"
-#git clone git://github.com/Lokaltog/vim-powerline.git
-#echo -ne "     \x1B[00;32mDONE\x1B[00m\n"
+echo -e "\n   * \x1B[00;36mPowerline\x1B[00m (cool statusline replacement)"
+echo "     by Kim Silkebækken"
+git clone git://github.com/Lokaltog/vim-powerline.git
+echo -ne "     \x1B[00;32mDONE\x1B[00m\n"
 
-#echo -e "\n   * \x1B[00;36mSignature\x1B[00m (pimped marks and signs)"
-#echo "     by kshenoy"
-#git clone git://github.com/kshenoy/vim-signature.git
-#echo -ne "     \x1B[00;32mDONE\x1B[00m\n"
+echo -e "\n   * \x1B[00;36mSignature\x1B[00m (pimped marks and signs)"
+echo "     by kshenoy"
+git clone git://github.com/kshenoy/vim-signature.git
+echo -ne "     \x1B[00;32mDONE\x1B[00m\n"
 
-#echo -e "\n   * \x1B[00;36mClang complete\x1B[00m (C/C++ completion using clang)"
-#echo "     by Xavier Deguillard"
-#git clone git://github.com/Rip-Rip/clang_complete.git
-#echo -ne "     \x1B[00;32mDONE\x1B[00m\n"
+echo -e "\n   * \x1B[00;36mClang complete\x1B[00m (C/C++ completion using clang)"
+echo "     by Xavier Deguillard"
+git clone git://github.com/Rip-Rip/clang_complete.git
+echo -ne "     \x1B[00;32mDONE\x1B[00m\n"
 
-#echo -e "\n   * \x1B[00;36mSupertab\x1B[00m (handy addition for text completion)"
-#echo "     by Eric Van Dewoestine"
-#git clone git://github.com/ervandew/supertab.git
-#echo -ne "     \x1B[00;32mDONE\x1B[00m\n"
+echo -e "\n   * \x1B[00;36mSupertab\x1B[00m (handy addition for text completion)"
+echo "     by Eric Van Dewoestine"
+git clone git://github.com/ervandew/supertab.git
+echo -ne "     \x1B[00;32mDONE\x1B[00m\n"
 
-#echo -e "\n   * \x1B[00;36mSnipMate\x1B[00m (snippets gallore!)"
-#echo "     originally by Michael Sanders but"
-#echo "     current package (and dependencies)"
-#echo "     by Rok Garbas (Tom Link, Marc Weber, Honza Pokorny)"
-###git://github.com/msanders/snipmate.vim.git
-#git clone git://github.com/garbas/vim-snipmate.git
-#git clone git://github.com/tomtom/tlib_vim.git
-#git clone git://github.com/MarcWeber/vim-addon-mw-utils.git
-#git clone git://github.com/honza/snipmate-snippets.git
-#echo -ne "     \x1B[00;32mDONE\x1B[00m\n"
+echo -e "\n   * \x1B[00;36mSnipMate\x1B[00m (snippets gallore!)"
+echo "     originally by Michael Sanders but"
+echo "     current package (and dependencies)"
+echo "     by Rok Garbas (Tom Link, Marc Weber, Honza Pokorny)"
+##git://github.com/msanders/snipmate.vim.git
+git clone git://github.com/garbas/vim-snipmate.git
+git clone git://github.com/tomtom/tlib_vim.git
+git clone git://github.com/MarcWeber/vim-addon-mw-utils.git
+git clone git://github.com/honza/snipmate-snippets.git
+echo -ne "     \x1B[00;32mDONE\x1B[00m\n"
 
-#echo -e "\n   * \x1B[00;36mGundo\x1B[00m (Looking for Lion's file versioning?)"
-#echo "     by Steve Losh"
-#git clone http://github.com/sjl/gundo.vim.git
-#echo -ne "     \x1B[00;32mDONE\x1B[00m\n"
+echo -e "\n   * \x1B[00;36mGundo\x1B[00m (Looking for Lion's file versioning?)"
+echo "     by Steve Losh"
+git clone http://github.com/sjl/gundo.vim.git
+echo -ne "     \x1B[00;32mDONE\x1B[00m\n"
 
-#echo -e "\n   * \x1B[00;36mOpenGL and GLSL syntax hl\x1B[00m (the missing highlighting)"
-#echo "     by Marc Costa"
-#git clone git://github.com/beyondmarc/opengl.vim.git
-#git clone git://github.com/beyondmarc/glsl.vim.git
-#echo -ne "     \x1B[00;32mDONE\x1B[00m\n"
-
-
-#echo -e "\n3) \x1B[00;34mCustomizing vim\n\x1B[00m"
-
-#echo -e "   * \x1B[00;36mPatching MacVim tab-switching shortcuts\x1B[00m"
-#echo "     to switch tabs to browser-like Ctrl+Tab / Ctrl+Shift+Tab"
-#echo "     This is a workaround: mapping <C-t> in .vimrc is not working"
-#patch ~/.vim/menu.vim < "$current_path"/menu.patch
-
-#echo -e "\n   * \x1B[00;36mSetting up a pimped .vimrc\x1B[00m"
-#echo "     assembled by me (Ferran Poveda)"
-#mv -f "$current_path"/vimrc $mvim_internals/vimrc
+echo -e "\n   * \x1B[00;36mOpenGL and GLSL syntax hl\x1B[00m (the missing highlighting)"
+echo "     by Marc Costa"
+git clone git://github.com/beyondmarc/opengl.vim.git
+git clone git://github.com/beyondmarc/glsl.vim.git
+echo -ne "     \x1B[00;32mDONE\x1B[00m\n"
 
 
-#echo -en "\n4) \x1B[00;34mCleaning cache, generating help tags, finishing...\x1B[00m"
-#vim +PowerlineClearCache +Helptags +q
+echo -e "\n3) \x1B[00;34mCustomizing vim\n\x1B[00m"
 
-#cd "$current_path"
-#echo -ne " \x1B[00;32m100%! Enjoy!\x1B[00m\n"
-#echo -e "\n\x1B[00;33mMore info in the README file, or at \x1B[00;34mhttp://github.com/fbeeper/fMacVim\x1B[00m"
-#echo -e "\x1B[00;33mAll comments, ideas, code or/and cheers will be appreciated!\x1B[00m\n" 
+echo -e "   * \x1B[00;36mPatching MacVim tab-switching shortcuts\x1B[00m"
+echo "     to switch tabs to browser-like Ctrl+Tab / Ctrl+Shift+Tab"
+echo "     This is a workaround: mapping <C-t> in .vimrc is not working"
+patch ~/.vim/menu.vim < "$current_path"/menu.patch
+
+echo -e "\n   * \x1B[00;36mSetting up a pimped .vimrc\x1B[00m"
+echo "     assembled by me (Ferran Poveda)"
+mv -f "$current_path"/vimrc $mvim_internals/vimrc
+
+
+echo -en "\n4) \x1B[00;34mCleaning cache, generating help tags, finishing...\x1B[00m"
+vim +PowerlineClearCache +Helptags +q
+
+cd "$current_path"
+echo -ne " \x1B[00;32m100%! Enjoy!\x1B[00m\n"
+echo -e "\n\x1B[00;33mMore info in the README file, or at \x1B[00;34mhttp://github.com/fbeeper/fMacVim\x1B[00m"
+echo -e "\x1B[00;33mAll comments, ideas, code or/and cheers will be appreciated!\x1B[00m\n" 
